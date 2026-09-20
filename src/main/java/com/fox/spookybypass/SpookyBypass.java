770ea7b129d4831eb58c17b21318bd7fce506327
To4noHe_BadBoy <essde342@gmail.com>
2026-09-20 15:04:21 +0300

    Add src/main/java/com/fox/spookybypass/SpookyBypass.java

---
+++ src/main/java/com/fox/spookybypass/SpookyBypass.java
@@ -0,0 +1,11 @@
+package com.fox.spookybypass;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SpookyBypass implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Project entrypoint.
+        // Keep gameplay/network behavior explicit and server-authoritative.
+    }
+}
